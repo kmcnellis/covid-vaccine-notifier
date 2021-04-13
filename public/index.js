99d6c4ccae69f6cd0c09d141a2f0fda5c9ddfda3
Yuchen Shi <yuchenshi@google.com>
2021-04-13 09:54:14 -0700

    Update public/index.js

--- public/index.js
+++ public/index.js
@@ -25,7 +25,7 @@
       }
     }
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://covid-spotter.web.app/subscribe");
+    xhr.open("POST", "/subscribe");
     xhr.setRequestHeader("Content-Type", "application/json;charset=utf-8");
     xhr.onload = function () {
       if (xhr.status >= 200 && xhr.status < 300) {
